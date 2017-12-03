a739ee5c5328a93f4c368acf570855fb8d922f69
Supriya34 <33888228+Supriya34@users.noreply.github.com>
2017-12-03 16:58:15 +0545

    Delete Number 2.py

--- Number 2.py
+++
@@ -1,6 +0,0 @@
-def area(radius = 0):
-    return (22/7)*radius*radius
-
-radius = float(input("Enter the radius of a circle "))
-
-print("The area of circle whose radius is {0} = {1:.3f} ".format(radius,area(radius)))
